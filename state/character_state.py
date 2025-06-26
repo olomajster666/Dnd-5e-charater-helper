d34284208daa95ea2ef3fe6f31a5f4e62c8e9858
Xires87 <frycmod@gmail.com>
2025-06-26 10:51:58 +0200

    character state can be created using existing data

--- state/character_state.py
+++ state/character_state.py
@@ -1,25 +1,27 @@
 class CharacterState:
-    def __init__(self):
-        self.data = {
-            "name": "",
-            "gender": "",
-            "race": None,
-            "class": None,
-            "background": None,
-            "stats": {
-                "strength": 0,
-                "dexterity": 0,
-                "constitution": 0,
-                "intelligence": 0,
-                "wisdom": 0,
-                "charisma": 0
-            },
-            "proficiencies": [],
-            "spells": [],
-            "equipment": [],
-            "level": 1,
-            "image": None
-        }
+
+    __defaultData = {
+        "name": "",
+        "gender": "",
+        "race": None,
+        "class": None,
+        "background": None,
+        "stats": {
+            "strength": 0,
+            "dexterity": 0,
+            "constitution": 0,
+            "intelligence": 0,
+            "wisdom": 0,
+            "charisma": 0
+        },
+        "proficiencies": [],
+        "spells": [],
+        "equipment": [],
+        "level": 1,
+        "image": None
+    }
+    def __init__(self, data : dict = __defaultData):
+        self.data = data
 
     def set(self, key, value):
         self.data[key] = value
